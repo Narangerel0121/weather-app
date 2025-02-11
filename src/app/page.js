21fd1820fe8f331c680a6b242a3392eaa1a840ea
Narangerel0121 <nrngrl0121@gmail.com>
2025-02-11 08:08:40 +0800

    edit

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,24 @@
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex">
 
-      <div className="flex">
-        <div className="h-screen w-1/2 bg-[#f3f4f6]"></div>
-        <div className="h-screen w-1/2 bg-[#0f141e]"></div>
-      </div>
-
-      <div>
-        <div className="w-[340px] h-[340px]  border-red-500 rounded-full"></div>
-      </div>
-
-      <div className="absoulte flex w-92 bg-pink-400 px-6 py-4 rounded-lg justify-start h-">
-        <img src="/assets/search.svg" className="w-6 h-6 ml-4" />
+      <div className="h-screen w-1/2 bg-[#f3f4f6]">
         <input className="" placeholder="Search" />
+        <div className="w-[414px] h-[828px] rounded-sm backdrop-blur-sm" >
+          <h3>Krakov</h3>
+          <img />
+          <h1>26</h1>
+          <p>Bright</p>
+          <div>
+            <img />
+            <img />
+            <img />
+            <img />
+          </div>
+        </div>
+      </div>
+
+      <div className="h-screen w-1/2 bg-[#0f141e]">
 
       </div>
 
